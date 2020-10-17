b8f6463090b0ddf4056e0e3fb04b3a3570409d8d
Julia Evans <julia@jvns.ca>
2020-10-17 10:21:11 -0400

    more examples

---
+++ 16-if-statements.sh
@@ -0,0 +1,12 @@
+echo "
+#########################
+## Example 16.1:        #
+## a basic if statement #
+#########################
+"
+
+# if can run any command. For example, touch!
+if touch files/test.txt
+then
+    echo 'did a thing!'
+fi
