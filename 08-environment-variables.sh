e73fa4966128cab8b71e5d2256c1352e8a62025e
Julia Evans <julia@jvns.ca>
2020-10-17 08:55:41 -0400

    way better headers

--- 08-environment-variables.sh
+++ 08-environment-variables.sh
@@ -1,43 +1,61 @@
 #!/bin/bash
 
-printf "Example 8.1\n===========\n"
+echo '
+################################################
+## Example 8.1:                                #
+## how to print  out all environment variables #
+################################################
+'
 
-echo; echo "let's print out some of your shell's environment variables"
-env | tail # piping to tail just because there are a LOT
-
-echo; echo "here's another way to get all environment variables that only works on Linux:"
+# the first way is `env`
+env | tail # I piped this to tail just because there are a LOT
+# here's another way to get environment variables that only works on linux:
 cat /proc/self/environ | tr '\0' '\n' | tail
 
-printf "\nExample 8.2\n===========\n"
-
-printf '\nboth shell variables and environment variables are accessed with $varname \n'
-# comment
-
+echo '
+#######################################
+## Example 8.2:                       #
+## printing out environment variables #
+## and shell variables                #
+#######################################
+'
+# we access them both the same way: $varname
 x="i'm a variable"
 echo "$x"
 echo "$HOME" # $HOME is an environment variable
 
-printf "\nExample 8.3\n===========\n"
+echo '
+########################################
+## Example 8.3:                        #
+## child processes inherit environment #
+## variables (but not shell variables) #
+########################################
+'
 
-echo 'child processes inherit environment variables'
+# child processes inherit environment variables
 
 export ENVVAR='panda'
-bash -c 'echo $ENVVAR' # this prints out 'panda'
+bash -c 'echo ENVVAR is: $ENVVAR' # this prints out 'panda'
 
-echo 'but not regular shell variables'
+# but not regular shell variables
 
-OTHERVAR='baby seal'
+SHELLVAR='baby seal'
 
-bash -c 'echo $OTHERVAR' # this doesn't print out anything
+bash -c 'echo SHELLVAR is: $SHELLVAR' # this doesn't print out anything
 
-printf "\nExample 8.4\n===========\n"
+echo '
+#######################################
+## Example 8.4:                       #
+## how to set an environment variable #
+## for a child process with env       #
+#######################################
+'
 
-echo "setting an environment variable just for a child process"
+env ANIMAL=porcupine bash -c 'echo in this child process, ANIMAL=$ANIMAL'
+# ANIMAL doesn't get set in the main process
+echo "but in the main process, ANIMAL=$ANIMAL"
 
-env ANIMAL=porcupine bash -c 'echo I have this variable: $ANIMAL'
-echo "the ANIMAL variable isn't set in our main shell: $ANIMAL"
+# it also works without the 'env'
 
-printf "\nit also works without the 'env'\n"
-
-ANIMAL='banana slug' bash -c 'echo I have this variable: $ANIMAL'
-echo "the ANIMAL variable still isn't set in our main shell: $ANIMAL"
+ANIMAL='banana slug' bash -c 'echo in the second child process, ANIMAL=$ANIMAL'
+echo "in the main process, we still have ANIMAL=$ANIMAL"
